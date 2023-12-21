bb7e40766b604a61fcf2ff97a84bef289be2cb8c
Amardeep <90225959+Amar033@users.noreply.github.com>
2023-12-21 22:52:12 +0530

    Update conststring.dart

--- lib/conststring.dart
+++ lib/conststring.dart
@@ -1,4 +1,4 @@
 class ConstStrings{
-  static const String clientId='1f6638cb19c746bc820829834509c28d';
-  static const String clientSecret='d0d3496e90694d62be15db8d33617c10';
+  static const String clientId='clientID';
+  static const String clientSecret='clientSecret';
 }   
